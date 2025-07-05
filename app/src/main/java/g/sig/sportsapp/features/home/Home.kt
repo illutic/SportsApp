64306bc513beb91e416fa0ad5497de3729acf630
George Sigalas <gsigalas@skroutz.gr>
2025-07-05 20:23:34 +0300

    feat: Enhance Home screen with TopAppBar and improved padding

--- app/src/main/java/g/sig/sportsapp/features/home/Home.kt
+++ app/src/main/java/g/sig/sportsapp/features/home/Home.kt
@@ -4,17 +4,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import g.sig.core.ui.ColorPalette
 import g.sig.core.ui.Spacing
 import g.sig.sportsapp.R
 import g.sig.sportsapp.features.home.components.sportscard.SportCard
@@ -25,6 +31,7 @@ import g.sig.sportsapp.features.home.mvi.HomeViewModel
 import g.sig.sportsapp.features.home.mvi.SportUiItem
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun HomeRoute(
     modifier: Modifier = Modifier,
@@ -51,13 +58,27 @@ internal fun HomeRoute(
 
     Scaffold(
         modifier = modifier,
+        containerColor = ColorPalette.SportsGray,
+        topBar = {
+            TopAppBar(
+                colors =
+                    TopAppBarDefaults.topAppBarColors(
+                        containerColor = ColorPalette.SportsBlue,
+                        titleContentColor = ColorPalette.SportsWhite,
+                    ),
+                title = { Text(stringResource(R.string.app_name)) },
+            )
+        },
         snackbarHost = { SnackbarHost(snackbarHostState) },
     ) {
         HomeScreen(
             state = homeState,
             onIntent = viewModel::sendIntent,
-            modifier = Modifier.padding(it),
-        )
+            modifier =
+                Modifier
+                    .padding(it)
+                    .padding(vertical = Spacing.padding_8),
+                )
     }
 }
 
