b3bcbbfe7c7a02fe73deb585d1814f8df17c3b93
George Sigalas <gsigalas@skroutz.gr>
2025-07-05 20:53:23 +0300

    feat: Implement pull-to-refresh functionality in Home screen

--- app/src/main/java/g/sig/sportsapp/features/home/Home.kt
+++ app/src/main/java/g/sig/sportsapp/features/home/Home.kt
@@ -1,12 +1,10 @@
 package g.sig.sportsapp.features.home
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -14,14 +12,18 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import g.sig.core.ui.ColorPalette
@@ -87,55 +89,70 @@ internal fun HomeRoute(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun HomeScreen(
     state: HomeState,
     onIntent: (HomeIntent) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(modifier) {
-        if (state.isLoading) {
-            CircularProgressIndicator(
-                modifier = Modifier.align(Center),
+    val pullToRefreshState = rememberPullToRefreshState()
+    PullToRefreshBox(
+        isRefreshing = state.isLoading,
+        state = pullToRefreshState,
+        indicator = {
+            PullToRefreshDefaults.Indicator(
+                state = pullToRefreshState,
+                isRefreshing = state.isLoading,
+                containerColor = ColorPalette.SportsWhite,
                 color = ColorPalette.SportsBlue,
+                modifier = Modifier.align(Alignment.TopCenter),
             )
-        }
+        },
+        onRefresh = { onIntent(HomeIntent.LoadSports) },
+        modifier = modifier,
+    ) {
+        HomeContent(
+            state = state,
+            onIntent = onIntent,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
 
+@Composable
+private fun HomeContent(
+    state: HomeState,
+    onIntent: (HomeIntent) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(Spacing.padding_8),
+    ) {
         if (state.isEmpty) {
-            Text(
-                text = stringResource(R.string.no_sports),
-                color = ColorPalette.SportsWhite,
-                modifier = Modifier.align(Center),
-            )
-        }
-
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(Spacing.padding_8),
-        ) {
-            items(state.sportUiItems) { sport ->
-                SportCard(
-                    state = sport,
-                    onSportFavorite = {
-                        onIntent(
-                            HomeIntent.ToggleFavoriteSport(
-                                it,
-                                !sport.getFavoritesOnly,
-                            ),
-                        )
-                    },
-                    onSportExpand = {
-                        onIntent(
-                            HomeIntent.ToggleExpandSport(
-                                it,
-                                !sport.isExpanded,
-                            ),
-                        )
-                    },
-                    onEventFavorite = { id, isFavorite ->
-                        onIntent(HomeIntent.ToggleFavoriteEvent(id, isFavorite))
-                    },
+            item {
+                Text(
+                    text = stringResource(R.string.no_sports),
+                    color = ColorPalette.SportsWhite,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxSize(),
                 )
             }
+        }
+        items(state.sportUiItems) { sport ->
+            SportCard(
+                state = sport,
+                onSportFavorite = {
+                    onIntent(HomeIntent.ToggleFavoriteSport(it, !sport.getFavoritesOnly))
+                },
+                onSportExpand = {
+                    onIntent(HomeIntent.ToggleExpandSport(it, !sport.isExpanded))
+                },
+                onEventFavorite = { id, isFavorite ->
+                    onIntent(HomeIntent.ToggleFavoriteEvent(id, isFavorite))
+                },
+            )
         }
     }
 }
