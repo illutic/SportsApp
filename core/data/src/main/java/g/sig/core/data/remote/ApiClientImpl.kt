68f451cba447b4d5b847f37188393f1eb083f388
George Sigalas <gsigalas@skroutz.gr>
2025-07-05 20:39:45 +0300

    feat: Update getSports method to use runCatching for improved error handling

--- core/data/src/main/java/g/sig/core/data/remote/ApiClientImpl.kt
+++ core/data/src/main/java/g/sig/core/data/remote/ApiClientImpl.kt
@@ -2,6 +2,7 @@ package g.sig.core.data.remote
 
 import g.sig.core.data.remote.enitites.SportApiDto
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
 import kotlinx.coroutines.CoroutineScope
 
@@ -11,5 +12,7 @@ internal class ApiClientImpl(
 ) : ApiClient,
     CoroutineScope by coroutineScope {
     override suspend fun getSports(): Result<List<SportApiDto>> =
-        httpClient.get("https://ios-kaizen.github.io/MockSports/sports.json").bodyResult()
+        runCatching {
+            httpClient.get("https://ios-kaizen.github.io/MockSports/sports.json").body()
+        }
 }
